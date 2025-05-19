d8784b24ef1b97758fa60a18b7a38862867478db
matthewyuh246 <matthewyuh246@gmail.com>
2025-05-19 23:58:34 +0900

    PNGFILE

---
+++ chapter3/PNGFile/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+)
+
+func dumpChunk(chunk io.Reader) {
+	var length int32
+	binary.Read(chunk, binary.BigEndian, &length)
+	buffer := make([]byte, 4)
+	chunk.Read(buffer)
+	fmt.Printf("chunk '%v' (%d bytes)\n", string(buffer), length)
+}
